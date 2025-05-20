8ce52fc1084a3a3938467edd27f29309a2a12689
Vitaliy Yatsenko <yatsiks69@gmail.com>
2025-05-20 14:43:37 +0300

    Clear history

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,12 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_ui_localizations/firebase_ui_localizations.dart';
 import 'package:fluffyhelpers_meditation/constants/app_button_styles.dart';
+import 'package:fluffyhelpers_meditation/constants/private_data.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:flutter_native_splash/flutter_native_splash.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
 import '../constants/app_colors.dart';
 import '../constants/app_routes.dart';
@@ -20,6 +22,10 @@ void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await dotenv.load(fileName: './dotenv');
   await Firebase.initializeApp(options: firebaseConfig);
+  await Supabase.initialize(
+    url: PrivateData.supabaseId,
+    anonKey: PrivateData.supabaseApi,
+  );
   runApp(const MyApp());
 }
 
@@ -32,41 +38,57 @@ class MyApp extends StatelessWidget {
       title: 'FluffyHelpers',
       theme: ThemeData(
         useMaterial3: true,
-        textTheme: TextTheme(
-          titleLarge: AppTextStyles.title,
-          titleMedium: AppTextStyles.title,
-          titleSmall: AppTextStyles.title,
-          bodyLarge: AppTextStyles.body,
-          bodyMedium: AppTextStyles.body,
-          bodySmall: AppTextStyles.body,
-          displayLarge: AppTextStyles.body,
-          displayMedium: AppTextStyles.body,
-          displaySmall: AppTextStyles.body,
-          headlineLarge: AppTextStyles.title,
-          headlineMedium: AppTextStyles.title,
-          headlineSmall: AppTextStyles.title,
-          labelLarge: AppTextStyles.body,
-          labelMedium: AppTextStyles.body,
-          labelSmall: AppTextStyles.body
-        ),
-        colorScheme: ColorScheme.fromSeed(
-          seedColor: AppColors.highlight,
-          surface: AppColors.primaryBackground,
-          //brightness: Brightness.light,
-        ),
-        inputDecorationTheme: InputDecorationTheme(
-          border: OutlineInputBorder(
-            borderRadius: BorderRadius.circular(8),
+          textTheme: TextTheme(
+            titleLarge: AppTextStyles.title,
+            titleMedium: AppTextStyles.title,
+            titleSmall: AppTextStyles.title,
+            bodyLarge: AppTextStyles.body,
+            bodyMedium: AppTextStyles.body,
+            bodySmall: AppTextStyles.body,
+            displayLarge: AppTextStyles.body,
+            displayMedium: AppTextStyles.body,
+            displaySmall: AppTextStyles.body,
+            headlineLarge: AppTextStyles.title,
+            headlineMedium: AppTextStyles.title,
+            headlineSmall: AppTextStyles.title,
+            labelLarge: AppTextStyles.body,
+            labelMedium: AppTextStyles.body,
+            labelSmall: AppTextStyles.body
           ),
-        ),
-        iconButtonTheme: IconButtonThemeData(
-          style: ButtonStyle(
-            foregroundColor: WidgetStateProperty.all(AppColors.text)
+          colorScheme: ColorScheme.fromSeed(
+            seedColor: AppColors.highlight,
+            surface: AppColors.primaryBackground,
+            brightness: Brightness.light,
+          ),
+          inputDecorationTheme: InputDecorationTheme(
+            border: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(8),
+              borderSide: const BorderSide(
+                  color: AppColors.highlight
+              )
+            ),
+            disabledBorder: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(8),
+              borderSide: const BorderSide(
+                  color: AppColors.highlight
+              )
+            ),
+            enabledBorder: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(8),
+              borderSide: const BorderSide(
+                  color: AppColors.highlight
+              )
+            ),
+            labelStyle: AppTextStyles.form,
+          ),
+          iconButtonTheme: IconButtonThemeData(
+            style: ButtonStyle(
+                foregroundColor: WidgetStateProperty.all(AppColors.text)
+            )
+          ),
+          elevatedButtonTheme: ElevatedButtonThemeData(
+            style: AppButtonStyles.secondary
           )
-        ),
-        elevatedButtonTheme: ElevatedButtonThemeData(
-          style: AppButtonStyles.secondary
-        )
       ),
       initialRoute: '/auth',
       onGenerateRoute: AppRoutes.onGenerateRoute,
--- lib/screens/library/library.dart
+++ lib/screens/library/library.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import '../../constants/app_colors.dart';
 import '../../constants/app_routes.dart';
 import '../../constants/app_text_styles.dart';
+import 'dialogs/new_playlist_dialog.dart';
 import 'mocks/main_category.mocks.dart';
 import 'mocks/sub_category.mocks.dart';
 import 'models/main_category.dart';
@@ -38,6 +39,16 @@ class _LibraryState extends State<Library> {
           .toList();
   });
 
+  void showNewPlaylistDialog(BuildContext context) {
+    showDialog(
+      context: context,
+      barrierDismissible: true,
+      builder: (BuildContext context) {
+        return const NewPlaylistDialog();
+      },
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -46,6 +57,23 @@ class _LibraryState extends State<Library> {
         backgroundColor: AppColors.primaryBackground,
         titleTextStyle: AppTextStyles.title,
         centerTitle: true,
+      ),
+      floatingActionButton: SizedBox(
+        width: 65,
+        height: 65,
+        child: FittedBox(
+          child: FloatingActionButton(
+            backgroundColor: AppColors.highlight, //TODO спитати за норм дизайн кнопки
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(20),
+              side: const BorderSide(color: AppColors.secondaryText),
+            ),
+            onPressed: () {
+              showNewPlaylistDialog(context);
+            },
+            child: const Icon(Icons.add_rounded, color: AppColors.text, size: 45,),
+          ),
+        ),
       ),
       backgroundColor: AppColors.primaryBackground,
       body: SafeArea(
@@ -62,7 +90,7 @@ class _LibraryState extends State<Library> {
                 child: SubCategoriesGridView(
                   filteredSubCategories: filteredSubCategories
                 )
-              )
+              ),
             ],
           ),
       )
@@ -115,68 +143,70 @@ class SubCategoriesGridView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return GridView.builder(
-      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-        crossAxisCount: (MediaQuery.of(context).size.width / 150).floor(),
-        crossAxisSpacing: 5,
-        mainAxisSpacing: 5,
-        mainAxisExtent: 600
-      ),
-      itemCount: filteredSubCategories.length,
-      scrollDirection: Axis.vertical,
-      itemBuilder: (context, index) {
-        return GestureDetector(
-          onTap: () {
-            Navigator.pushNamed(context, AppRoutes.subCategoryDetails,
-              arguments: filteredSubCategories[index]);
-          },
-          child: Card(
-            elevation: 6,
-            color: AppColors.secondaryBackground,
-            child: Padding(
-              padding: const EdgeInsets.all(0),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.stretch,
-                children: [
-                  Expanded(
-                    flex: 3,
-                    child: ClipRRect(
-                      borderRadius: const BorderRadius.vertical(
-                        top: Radius.circular(12)),
-                      child: Image.asset(
-                        filteredSubCategories[index].pathToImage,
-                        fit: BoxFit.cover,
-                      ),
-                    )
-                  ),
-                  const SizedBox(height: 20),
-                  Flexible(
-                    flex: 1,
-                    child: Text(
-                      filteredSubCategories[index].name,
-                      style: AppTextStyles.title,
-                      textAlign: TextAlign.center,
-                      overflow: TextOverflow.fade,
-                      softWrap: true,
-                    )
-                  ),
-                  const SizedBox(height: 10),
-                  Flexible(
-                    flex: 2,
-                    child: Text(
-                      filteredSubCategories[index].description,
-                      style: AppTextStyles.body,
-                      textAlign: TextAlign.center,
-                      overflow: TextOverflow.fade,
-                      softWrap: true,
-                    )
-                  ),
-                ]
+    return Scaffold(
+      body: GridView.builder(
+        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: (MediaQuery.of(context).size.width / 150).floor(),
+          crossAxisSpacing: 5,
+          mainAxisSpacing: 5,
+            mainAxisExtent: 600
+        ),
+        itemCount: filteredSubCategories.length,
+        scrollDirection: Axis.vertical,
+        itemBuilder: (context, index) {
+          return GestureDetector(
+            onTap: () {
+              Navigator.pushNamed(context, AppRoutes.subCategoryDetails,
+                arguments: filteredSubCategories[index]);
+            },
+            child: Card(
+              elevation: 6,
+              color: AppColors.secondaryBackground,
+              child: Padding(
+                padding: const EdgeInsets.all(0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: [
+                    Expanded(
+                      flex: 3,
+                      child: ClipRRect(
+                        borderRadius: const BorderRadius.vertical(
+                          top: Radius.circular(12)),
+                        child: Image.asset(
+                          filteredSubCategories[index].pathToImage,
+                          fit: BoxFit.cover,
+                        ),
+                      )
+                    ),
+                    const SizedBox(height: 20),
+                    Flexible(
+                      flex: 1,
+                      child: Text(
+                        filteredSubCategories[index].name,
+                        style: AppTextStyles.title,
+                        textAlign: TextAlign.center,
+                        overflow: TextOverflow.fade,
+                        softWrap: true,
+                      )
+                    ),
+                    const SizedBox(height: 10),
+                    Flexible(
+                      flex: 2,
+                      child: Text(
+                        filteredSubCategories[index].description,
+                        style: AppTextStyles.body,
+                        textAlign: TextAlign.center,
+                        overflow: TextOverflow.fade,
+                        softWrap: true,
+                      )
+                    ),
+                  ]
+                ),
               ),
             ),
-          ),
-        );
-      }
+          );
+        }
+      ),
     );
   }
 }
