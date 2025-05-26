8275c14fc05360b8dbb2538eee7eaa466bfb787c
Sofiia Kyrychok <hentaionelove2005@gmail.com>
2025-05-26 20:09:54 +0300

    Add constructor foundation
    Added main buttons, icons, changed the accent color. General page layout and saving sound states

--- lib/constants/app_colors.dart
+++ lib/constants/app_colors.dart
@@ -14,7 +14,7 @@ class AppColors {
   static const Color primaryBackground = Color(0xFF0D1B2A);
   static const Color secondaryBackground = Color(0xFF1B263B);
   static const Color highlight = Color(0xFF415A77);
-  static const Color accent = Color(0xFF527BA0);
+  static const Color accent = Color(0xFF5a86ad);
   static const Color text = Color(0xFFE0E1DD);
   static const Color secondaryText = Color(0xFFC9D6DF);
   static const Color error = Color(0xFFD32C16);
--- lib/screens/constructor/constructor.dart
+++ lib/screens/constructor/constructor.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
-
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import '../../constants/app_form_styles.dart';
+import '../../constants/app_colors.dart';
 import '../../global_widgets/custom_app_bar.dart';
 import '../../l10n/app_localizations.dart';
 
@@ -11,15 +13,137 @@ class Constructor extends StatefulWidget {
 }
 
 class _ConstructorState extends State<Constructor> {
+  List<bool> buttonStates = List.generate(20, (_) => false);
+
+  final List<IconData> icons = [
+    FontAwesomeIcons.dove,
+    Icons.music_note,
+    Icons.park,
+    Icons.eco,
+    Icons.water,
+    FontAwesomeIcons.fire,
+    Icons.air,
+    Icons.flash_on,
+    Icons.grass,
+    Icons.pets,
+    FontAwesomeIcons.guitar,
+    Icons.record_voice_over,
+    Icons.train,
+    Icons.library_music,
+    Icons.audiotrack,
+    FontAwesomeIcons.music,
+    Icons.mic,
+    FontAwesomeIcons.feather,
+    FontAwesomeIcons.sun,
+    FontAwesomeIcons.om
+  ];
+
+  void _onGridButtonPressed(int index) {
+    setState(() {
+      buttonStates[index] = !buttonStates[index];
+    });
+
+    String soundId = 'sound_$index';
+    print('Pressed $soundId');
+  }
+
+  void _showInputDialog(BuildContext context) {
+    final TextEditingController _controller = TextEditingController();
+
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: const Text('Зберегти композицію'),
+          backgroundColor: AppColors.secondaryBackground,
+          content: TextField(
+            controller: _controller,
+            decoration: AppFormStyles.formInputDecorationDefault(
+              labelText: 'Введіть назву композиції',
+              isError: false
+            ),
+          ),
+          actions: [
+            ElevatedButton(
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+              child: const Text('Скасувати'),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                String value = _controller.text;
+                print('Введено: $value');
+                Navigator.of(context).pop(); //TODO: додати передачу значення
+              },
+              child: const Text('OK'),
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+
   @override
   Widget build(BuildContext context) {
-    final localizations = AppLocalizations.of(context)!;
-    final String titleText = localizations.constructorTitle;
-
     return Scaffold(
-      appBar: CustomAppBar(title: titleText, leading: null,),
-      body: const Column(
+      body: Column(
         children: [
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.all(8),
+              child:GridView.count(
+                crossAxisCount: 3,
+                mainAxisSpacing: 12,
+                crossAxisSpacing: 12,
+                children: List.generate(20, (index) {
+                  final icon = icons[index % icons.length];
+                  final isActive = buttonStates[index];
+                  return Center(
+                    child: Container(
+                      width: 100,
+                      height: 100,
+                      decoration: BoxDecoration(
+                        shape: BoxShape.circle,
+                        color: isActive ? AppColors.highlight : Colors.transparent,
+                        border: Border.all(color: AppColors.accent, width: 3),
+                      ),
+                      child: IconButton(
+                        iconSize: 50,
+                        icon: Icon(icon, color: AppColors.accent),
+                        onPressed: () {
+                          _onGridButtonPressed(index);
+                        },
+                      ),
+                    ),
+                  );
+                }),
+              ),
+            ),
+          ),
+
+          Container(
+            padding: EdgeInsets.symmetric(vertical: 10, horizontal: 8),
+            color: AppColors.secondaryBackground,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                ElevatedButton(
+                  onPressed: () => print('Action 1'),
+                  child: const Icon(Icons.settings, size: 36),
+                ),
+                ElevatedButton(
+                  onPressed: () => _showInputDialog(context),
+                  child: const Icon(Icons.add, size: 36),
+                ),
+                ElevatedButton(
+                  onPressed: () => print('Action 3'),
+                  child: const Icon(Icons.favorite, size: 36),
+                ),
+              ],
+            ),
+          ),
         ],
       ),
     );
